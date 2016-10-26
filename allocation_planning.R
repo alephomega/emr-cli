525f6e3e966d6be90cf440cd4652f3769f6a6e98
hanadmin <hanadmin@on-data-etl1>
2016-10-26 19:37:48 +0900

    Add service argument

--- allocation_planning.R
+++ allocation_planning.R
@@ -1,11 +1,51 @@
+#!/usr/bin/Rscript --vanilla
+
+
+basedir <- function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  m <- grep(needle, args)
+  if (length(m) > 0) {
+    f <- normalizePath(sub(needle, "", args[m]))
+  } else {
+    f <- normalizePath(sys.frames()[[1]]$ofile)
+  }
+  
+  dirname(f)
+}
+
+arg <- function(name) {
+  args <- commandArgs(TRUE)
+  
+  t0 <- NA
+  m <- match(sprintf("--%s", name), args, 0L)
+  if (m) {
+    t0 <- args[m + 1]
+  }
+  
+  t0
+}
+
+
+setwd(basedir())
 source("spark_submit.R")
 
-run <- function(cluster, jar, interval = 300000L, timeslot, redis) {
+TIMESLOT_LENGTH <- 300000L
+TIMESLOT_LENGTH_IN_SECONDS <- TIMESLOT_LENGTH / 1000L
+timeslot <- function(sl) {
+  ct <-as.POSIXct(Sys.time(), tz = "UTC")
+  format(
+    as.POSIXct(floor((as.double(ct) - sl) / sl) * sl, tz = "UTC", origin = "1970-01-01"),
+    format ="%Y%m%d%H%M",
+    tz = "UTC")
+}
+
+run <- function(cluster, jar, interval = TIMESLOT_LENGTH, timeslot, redis, service) {
   args <- c("--interval", as.integer(interval), 
             "--timeSlot", timeslot, 
             "--redisHost", redis$host, 
-            "--redisPort", as.integer(redis$port))
-  
+            "--redisPort", as.integer(redis$port),
+            "--service", service) 
   s <- spec(
     name = sprintf("Allocation Planning - %s", timeslot), 
     args = args, 
@@ -16,3 +56,12 @@ run <- function(cluster, jar, interval = 300000L, timeslot, redis) {
   
   submit(cluster, s)
 }
+
+ts <- arg("timeSlot")
+ts <- if (is.na(ts)) timeslot(TIMESLOT_LENGTH_IN_SECONDS) else ts
+
+run(cluster = arg("cluster"),
+    jar = arg("jar"),
+    timeslot = ts,
+    redis = list(host = arg("redisHost"), port = as.integer(arg("redisPort"))),
+	service = arg("service"))
